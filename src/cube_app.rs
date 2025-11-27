07e9deed35825311e35bf618d8eb29bb5ba7954f
Muten-Roshi-Sama <21245@ecam.be>
2025-11-27 15:51:39 +0100

    Implemented wgpu-bootstrap cube rendering.

---
+++ src/cube_app.rs
@@ -0,0 +1,283 @@
+use wgpu_bootstrap::{
+    cgmath, egui,
+    util::orbit_camera::{CameraUniform, OrbitCamera},
+    wgpu::{self, util::DeviceExt},
+    App, Context,
+};
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.5, 0.5, 0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, 0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    ///////////////////////////////////
+    Vertex {
+        position: [0.5, 0.5, 0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, -0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, 0.5, -0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    //////////////////////////////
+    Vertex {
+        position: [0.5, 0.5, -0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, -0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, -0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, -0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+    //////////////////////////////
+    Vertex {
+        position: [-0.5, 0.5, 0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, -0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, -0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    //////////////////////////////
+    Vertex {
+        position: [0.5, 0.5, 0.5],
+        color: [0.0, 1.0, 1.0],
+    },
+    Vertex {
+        position: [0.5, 0.5, -0.5],
+        color: [0.0, 1.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, -0.5],
+        color: [0.0, 1.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, 0.5, 0.5],
+        color: [0.0, 1.0, 1.0],
+    },
+    //////////////////////////////
+    Vertex {
+        position: [0.5, -0.5, 0.5],
+        color: [1.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.5],
+        color: [1.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, -0.5],
+        color: [1.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, -0.5],
+        color: [1.0, 0.0, 1.0],
+    },
+];
+
+#[rustfmt::skip]
+const INDEXES: &[u32] = &[
+     0,  1,  2,  0,  2,  3,
+     4,  5,  6,  4,  6,  7,
+     8,  9, 10,  8, 10, 11,
+    12, 13, 14, 12, 14, 15,
+    16, 17, 18, 16, 18, 19,
+    20, 21, 22, 20, 22, 23,
+];
+
+pub struct CubeApp {
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    render_pipeline: wgpu::RenderPipeline,
+    num_indices: u32,
+    camera: OrbitCamera,
+}
+
+impl CubeApp {
+    pub fn new(context: &Context) -> Self {
+        let index_buffer = context
+            .device()
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDEXES),
+                usage: wgpu::BufferUsages::INDEX,
+            });
+
+        let num_indices = INDEXES.len() as u32;
+
+        let vertex_buffer =
+            context
+                .device()
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer"),
+                    contents: bytemuck::cast_slice(VERTICES),
+                    usage: wgpu::BufferUsages::VERTEX,
+                });
+
+        let camera_bind_group_layout = context
+            .device()
+            .create_bind_group_layout(&CameraUniform::desc());
+
+        let shader = context
+            .device()
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Shader"),
+                source: wgpu::ShaderSource::Wgsl(include_str!("../shaders/cube_shader.wgsl").into()),
+            });
+
+        let pipeline_layout =
+            context
+                .device()
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[&camera_bind_group_layout],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline =
+            context
+                .device()
+                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                    label: Some("Render Pipeline"),
+                    layout: Some(&pipeline_layout),
+                    vertex: wgpu::VertexState {
+                        module: &shader,
+                        entry_point: "vs_main",
+                        buffers: &[Vertex::desc()],
+                        compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    },
+                    fragment: Some(wgpu::FragmentState {
+                        module: &shader,
+                        entry_point: "fs_main",
+                        targets: &[Some(wgpu::ColorTargetState {
+                            format: context.format(),
+                            blend: Some(wgpu::BlendState::REPLACE),
+                            write_mask: wgpu::ColorWrites::ALL,
+                        })],
+                        compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    }),
+                    primitive: wgpu::PrimitiveState {
+                        topology: wgpu::PrimitiveTopology::TriangleList,
+                        strip_index_format: None,
+                        front_face: wgpu::FrontFace::Ccw,
+                        cull_mode: Some(wgpu::Face::Back),
+                        // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                        polygon_mode: wgpu::PolygonMode::Fill,
+                        // Requires Features::DEPTH_CLIP_CONTROL
+                        unclipped_depth: false,
+                        // Requires Features::CONSERVATIVE_RASTERIZATION
+                        conservative: false,
+                    },
+                    depth_stencil: Some(wgpu::DepthStencilState {
+                        format: context.depth_stencil_format(),
+                        depth_write_enabled: true,
+                        depth_compare: wgpu::CompareFunction::Less,
+                        stencil: wgpu::StencilState::default(),
+                        bias: wgpu::DepthBiasState::default(),
+                    }),
+                    multisample: wgpu::MultisampleState {
+                        count: 1,
+                        mask: !0,
+                        alpha_to_coverage_enabled: false,
+                    },
+                    multiview: None,
+                    cache: None,
+                });
+
+        let mut camera = OrbitCamera::new(
+            context,
+            45.0,
+            context.size().x / context.size().y,
+            0.1,
+            100.0,
+        );
+        camera
+            .set_target(cgmath::point3(0.0, 0.0, 0.0))
+            .set_polar(cgmath::point3(2.0, 0.0, 0.0))
+            .update(context);
+
+        Self {
+            vertex_buffer,
+            index_buffer,
+            render_pipeline,
+            num_indices,
+            camera,
+        }
+    }
+}
+
+impl App for CubeApp {
+    fn input(&mut self, input: egui::InputState, context: &Context) {
+        self.camera.input(input, context);
+    }
+
+    fn render(&self, render_pass: &mut wgpu::RenderPass<'_>) {
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+        render_pass.set_bind_group(0, self.camera.bind_group(), &[]);
+        render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod cube_app;
+
+use std::sync::Arc;
+
+use crate::cube_app::CubeApp;
+use wgpu_bootstrap::{egui, Runner};
+
 fn main() {
-    println!("Hello, world!");
+    let mut runner = Runner::new(
+        "Cube App",
+        800,
+        600,
+        egui::Color32::from_rgb(245, 245, 245),
+        32,
+        0,
+        Box::new(|context| Arc::new(CubeApp::new(context))),
+    );
+    runner.run();
 }
